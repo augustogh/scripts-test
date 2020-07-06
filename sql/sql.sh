36ea40fc30577a07acfccd479f9b67e127994eb3
augustogh <augusto.lobelos@gmail.com>
2020-07-06 02:25:55 +0000

    volumes

---
+++ sql/sql.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Start a mssql-server instance running as the SQL Express edition
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=pandora_sql' -e 'MSSQL_PID=Express' -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest-ubuntu 
